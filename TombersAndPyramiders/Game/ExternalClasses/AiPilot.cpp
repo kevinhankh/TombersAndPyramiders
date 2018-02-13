009adac4592217f083e82fb8e0a2a11843bd218c
Chen <q1002608870@gmail.com>
2018-02-13 00:17:06 -0800

    Fixed AIPilot and Monster AI

--- TombersAndPyramiders/Game/ExternalClasses/AiPilot.cpp
+++ TombersAndPyramiders/Game/ExternalClasses/AiPilot.cpp
@@ -9,19 +9,19 @@ int attackCount = 0;
 int attackNumber = rand();
 void AiPilot::shouldFire()
 {
-	attackNumber = attackNumber %6;
+	attackNumber = attackNumber % 6;
 	attackCount = attackCount + 1;
 	if (attackCount == 120)
 	{
 		attackNumber = rand();
 		attackCount = 0;
 	}
-		if (attackNumber == 1)
-		{
-			currentState = attack;
-			//attackNumber = rand();
-		}
-	
+	if (attackNumber == 1)
+	{
+		currentState = attack;
+		//attackNumber = rand();
+	}
+
 }
 
 void AiPilot::setController(BaseController* controller)
@@ -64,7 +64,7 @@ int coun = 0;
 int randomNumber = rand();
 Vector2 AiPilot::getMovement()
 {
-	randomNumber = randomNumber %9;
+	randomNumber = randomNumber % 9;
 	coun = coun + 1;
 	Vector2 movement = Vector2(0, 0);
 	if (coun == 60)
@@ -72,43 +72,43 @@ Vector2 AiPilot::getMovement()
 		randomNumber = rand();
 		coun = 0;
 	}
-		if (randomNumber == 1)
-		{
-			movement.setX(-0.2f);
-		}
-		else if (randomNumber == 2)
-		{
-			movement.setX(0.2f);
-	
-		}
-		else if (randomNumber == 3)
-		{
-			movement.setY(0.2f);
-		}
-		else if (randomNumber == 4)
-		{
-			movement.setY(-0.2f);
-		}
-		else if (randomNumber == 5)
-		{
-			movement.setX(-0.2f);
-			movement.setY(0.2f);
-		}
-		else if (randomNumber == 6)
-		{
-			movement.setX(0.2f);
-			movement.setY(0.2f);
-		}
-		else if (randomNumber == 7)
-		{
-			movement.setX(-0.2f);
-			movement.setY(-0.2f);
-		}
-		else if (randomNumber == 8)
-		{
-			movement.setX(0.2f);
-			movement.setY(-0.2f);
-		}
-	
+	if (randomNumber == 1)
+	{
+		movement.setX(-0.2f);
+	}
+	else if (randomNumber == 2)
+	{
+		movement.setX(0.2f);
+
+	}
+	else if (randomNumber == 3)
+	{
+		movement.setY(0.2f);
+	}
+	else if (randomNumber == 4)
+	{
+		movement.setY(-0.2f);
+	}
+	else if (randomNumber == 5)
+	{
+		movement.setX(-0.2f);
+		movement.setY(0.2f);
+	}
+	else if (randomNumber == 6)
+	{
+		movement.setX(0.2f);
+		movement.setY(0.2f);
+	}
+	else if (randomNumber == 7)
+	{
+		movement.setX(-0.2f);
+		movement.setY(-0.2f);
+	}
+	else if (randomNumber == 8)
+	{
+		movement.setX(0.2f);
+		movement.setY(-0.2f);
+	}
+
 	return movement;
 }
