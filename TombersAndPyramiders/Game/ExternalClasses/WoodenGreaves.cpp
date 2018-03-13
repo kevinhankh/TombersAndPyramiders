5a0b441f2ab4d8ddfeb47733d05fcce10f7c50eb
Erick FdA <erick.fernandez.da@gmail.com>
2018-03-13 09:52:52 -0700

    Tweaked some values.

--- TombersAndPyramiders/Game/ExternalClasses/WoodenGreaves.cpp
+++ TombersAndPyramiders/Game/ExternalClasses/WoodenGreaves.cpp
@@ -18,8 +18,8 @@
 /*----------------------------------------------------------------------------------------
 Static Fields
 ----------------------------------------------------------------------------------------*/
-const float WoodenGreaves::WOODEN_GREAVES_COOLDOWN_TIME = 0.5f;
-const float WoodenGreaves::WOODEN_GREAVES_DASH_DURATION = 3.0f;
+const float WoodenGreaves::WOODEN_GREAVES_COOLDOWN_TIME = 1.0f;
+const float WoodenGreaves::WOODEN_GREAVES_DASH_DURATION = 0.5f;
 const float WoodenGreaves::WOODEN_GREAVES_MAX_DASH_SPEED = 100.0f;
 
 /*----------------------------------------------------------------------------------------
@@ -42,13 +42,20 @@ void WoodenGreaves::onStart()
 
 void WoodenGreaves::effect(int ticks)
 {
-	auto dashRotation = owner()->getTransform()->getRotation();
-	auto dashAmount = ((float)ticks / 1000) * WOODEN_GREAVES_MAX_DASH_SPEED;
-	auto dashDirection = Vector2(1, 0);
-	dashDirection.rotate(dashRotation);
-	Vector2* pDashDuration = dashDirection * dashAmount;
+	if (m_timeLeftInDash > 0)
+	{
+		auto dashRotation = owner()->getTransform()->getRotation();
+		auto dashAmount = ((float)ticks / 1000) * WOODEN_GREAVES_MAX_DASH_SPEED;
+		//auto lerpFactor = (WOODEN_GREAVES_DASH_DURATION - m_timeLeftInDash) / WOODEN_GREAVES_DASH_DURATION;
+		//dashAmount *= (1 - lerpFactor);
 
-	//owner()->getTransform()->addTranslation(pDashDuration->getX(), pDashDuration->getY());
-	//owner()->getComponent<Rigidbody>()->setVelocity(*pDashDuration);
-	owner()->getComponent<CharacterController>()->move(*pDashDuration);
+		auto dashDirection = Vector2(1, 0);
+		dashDirection.rotate(dashRotation);
+		Vector2* pDashDuration = dashDirection * dashAmount;
+
+		//owner()->getTransform()->addTranslation(pDashDuration->getX(), pDashDuration->getY());
+		//owner()->getComponent<Rigidbody>()->setVelocity(*pDashDuration);
+		owner()->getComponent<CharacterController>()->move(*pDashDuration);
+		m_timeLeftInDash -= (float)ticks / 1000.0f;
+	}
 }
