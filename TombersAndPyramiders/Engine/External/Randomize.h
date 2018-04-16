5996e729e765480f4cd34b418dff8dff61c08638
Jaegar Sarauer <jaegars@hotmail.com>
2018-04-15 21:07:11 -0700

    New randomize implemented. Working on windows.

--- TombersAndPyramiders/Engine/External/Randomize.h
+++ TombersAndPyramiders/Engine/External/Randomize.h
@@ -1,26 +1,13 @@
 #pragma once
 
-#include <boost/random/mersenne_twister.hpp>
-#include <boost/random/uniform_int.hpp>
-#include <boost/random/variate_generator.hpp>
-
-
 class Randomize {
     private:
-        static boost::mt19937 gen;
-        const static int MAX_INT = 2147483647;
+		const static int MAX_INT;
     
     public:
+		static void SetSeed (int seed);
 
-    static void SetSeed(int seed) {
-        gen.seed(seed);
-        //rng.distribution().reset();
-    }
+		static int Random (int min = 0, int max = MAX_INT);
 
-    static int Random(int min = 0, int max = MAX_INT) {
-        boost::random::uniform_int_distribution<> dist(min, max);
-        return dist(gen);
-    }
+		static float RandomFloat (int min = 0, int max = MAX_INT);
 };
-
-boost::mt19937 Randomize::gen;
