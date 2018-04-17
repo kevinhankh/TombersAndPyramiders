7aa0ed99f548c73b7041fa7b6d3540391f3fb37f
CarsonRoscoe <CarsonRoscoe7@gmail.com>
2018-04-16 22:47:46 -0700

    On ESQ button mid-game hug dialog appears confirm quit decision

--- TombersAndPyramiders/Game/Scenes/NetworkedGameScene.cpp
+++ TombersAndPyramiders/Game/Scenes/NetworkedGameScene.cpp
@@ -15,12 +15,11 @@
 #include "GameManager.h"
 #include "FogOfWarCamera.h"
 #include "GhostCamera.h"
+#include "InputManager.h"
+#include "MainMenuScene.h"
 
-std::map<int, shared_ptr<Character>> players;
-std::shared_ptr<Character> Ai = nullptr;
-std::shared_ptr<Character> Ai2 = nullptr;
-std::shared_ptr<Character> Ai3 = nullptr;
-std::shared_ptr<Character> Ai4 = nullptr;
+bool menuVisible = false;
+std::shared_ptr<SimpleSprite> escapeMenu = nullptr;
 
 NetworkedGameScene::NetworkedGameScene ()
 {
@@ -31,211 +30,8 @@ void NetworkedGameScene::onStart ()
 {
 	GameManager::getInstance()->resizeQuadTree(0, 0, 200, 200);
 	Camera::getActiveCamera()->setActiveCamera(GameManager::getInstance()->createGameObject<FogOfWarCamera>(true));
-
-	//time_t mapSeedID = time(NULL);
-	//srand(mapSeedID);
-	//GeneratorManager::getInstance()->generateLevel(30, 30, 2, 0);
-	
-	//GeneratorManager::getInstance()->drawLevel(0);
-
 	SpawnManager::getInstance()->generateMiscSquare(25, -25, -100, 200, "ControlsBlank.png", false);
-	/*
-	const float scale = 5;
-
-	SpawnManager::getInstance()->generateMiscSquare(25, -25, -100, 115, "sandBG.png", false);
-
-	/*std::vector<std::vector<int>> level{ {0,0,0,0},
-	{0,0,0,0},
-	{0,0,0,0},
-	{0,0,0,0} };
-
-	std::vector<std::vector<int>> level{{ 1,5,5,5,5,2,9,1,5,5,5,5,5,5,5,5,5,2 } ,
-										{ 7,0,0,0,0,3,5,4,0,0,0,0,0,0,0,0,0,8 } ,
-										{ 7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8 } ,
-										{ 7,0,0,0,0,1,6,2,0,0,0,0,0,0,0,0,0,8 } ,
-										{ 3,6,2,0,1,4,9,7,0,0,0,0,0,0,0,0,0,8 } ,
-										{ 9,9,7,0,8,9,9,7,0,0,0,0,0,0,0,0,0,8 } ,
-										{ 1,5,4,0,3,5,2,7,0,0,0,0,0,0,0,0,0,8 } ,
-										{ 7,0,0,0,0,0,8,7,0,0,0,0,0,0,0,0,0,8 } ,
-										{ 7,0,0,0,0,0,8,3,6,6,6,6,6,2,0,0,0,8 } ,
-										{ 7,0,0,0,0,0,8,9,9,9,9,9,9,7,0,0,0,8 } ,
-										{ 3,6,6,6,6,6,4,9,9,9,9,9,9,7,0,0,0,8 } ,
-										{ 1,5,5,5,5,5,5,5,5,5,5,5,5,4,0,0,0,8 } ,
-										{ 7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8 } ,
-										{ 7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8 } ,
-										{ 3,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,4 } };
-
-	for (int i = 0; i < level.size(); i++)
-	{
-		for (int j = 0; j < level[i].size(); j++)
-		{
-			switch (level[i][j]) {
-			case 0:
-				SpawnManager::getInstance()->generateMiscSquare(j*scale, -i*scale, -10, scale, "stoneTile.png", true);
-				break;
-			case 1:
-				SpawnManager::getInstance()->generateMiscSquare(j*scale, -i*scale, -10, scale, "wallTopLeft.png", true);
-				break;
-			case 2:
-				SpawnManager::getInstance()->generateMiscSquare(j*scale, -i*scale, -10, scale, "wallTopRight.png", true);
-				break;
-			case 3:
-				SpawnManager::getInstance()->generateMiscSquare(j*scale, -i*scale, -10, scale, "wallBottomLeft.png", true);
-				break;
-			case 4:
-				SpawnManager::getInstance()->generateMiscSquare(j*scale, -i*scale, -10, scale, "wallBottomRight.png", true);
-				break;
-			case 5:
-				SpawnManager::getInstance()->generateMiscSquare(j*scale, -i*scale, -10, scale, "wallTop.png", true);
-				break;
-			case 6:
-				SpawnManager::getInstance()->generateMiscSquare(j*scale, -i*scale, -10, scale, "wallBottom.png", true);
-				break;
-			case 7:
-				SpawnManager::getInstance()->generateMiscSquare(j*scale, -i*scale, -10, scale, "wallLeft.png", true);
-				break;
-			case 8:
-				SpawnManager::getInstance()->generateMiscSquare(j*scale, -i*scale, -10, scale, "wallRight.png", true);
-				break;
-			}
-		}
-	}
-
-	*/
-
-	/*
-	const float size = 12;
-	const float scale = 5;
-
-
-	SpawnManager::getInstance ()->generateMiscSquare (25, -25, -100, 115, "sandBG.png", false);
-=======
-	
-
-	SpawnManager::getInstance()->generateMiscSquare(25, -25, -100, 115, "sandBG.png", false);
-	AudioManager::getInstance()->playMusic(MUSIC_LEVEL_1);
->>>>>>> 5c2ae033902e886b31a700b29d0dcc98fa50e94e
-
-	for (float x = 0; x <= size; x++)
-	{
-		for (float y = 0; y >= -size; y--)
-		{
-			float column = x * scale;
-			float row = y * scale;
-			float floorOffset = 0.56f;
-			float wallOffset = 0.0f;
-
-<<<<<<< HEAD
-			// Create a floor tile. 
-			SpawnManager::getInstance ()->generateMiscSquare (column, row - (y * floorOffset), (y * -1) - 50, scale, "stoneTile.png", false);
-
-			int xMod = (int)x % 3;
-			int yMod = (int)y % 3;
-
-
-			if (xMod == 0 && yMod == 0 && x != 0 && y != 0 && x != size && y != -size)
-			{
-				SpawnManager::getInstance ()->generateMiscSquare (column, row - (y * floorOffset), 50, scale, "table.png", true);
-			}
-
-			// Create our walls. A wall is half the size of a tile, so we need two. Each wall that gets created south of a previous wall
-			// needs to have a higher z value so it ovelaps the previous wall.
-			// Create top walls.
-=======
-			SpawnManager::getInstance()->generateMiscSquare(column, row - (y * floorOffset), (y * -1) - 50, scale, "stoneTile.png", false);
-
->>>>>>> 5c2ae033902e886b31a700b29d0dcc98fa50e94e
-			if (y == 0)
-			{
-				// Create top left wall.
-				if (x == 0)
-				{
-<<<<<<< HEAD
-					SpawnManager::getInstance ()->generateMiscSquare (column, row + (scale / 2) + floorOffset, y * -1, scale, "wallTopLeft.png", true);
-					SpawnManager::getInstance ()->generateMiscSquare (column - scale + wallOffset, row + floorOffset, (y * -1) + 0.5, scale, "wallLeft.png", true);
-=======
-					SpawnManager::getInstance()->generateMiscSquare(column, row, y * -1, scale, "wallTopLeft_Edge.png", true);
->>>>>>> 5c2ae033902e886b31a700b29d0dcc98fa50e94e
-				}
-				// Create top right wall.
-				else if (x == size)
-				{
-<<<<<<< HEAD
-					SpawnManager::getInstance ()->generateMiscSquare (column, row + (scale / 2) + floorOffset, y * -1, scale, "wallTopRight.png", true);
-					SpawnManager::getInstance ()->generateMiscSquare (column + scale - wallOffset, row + floorOffset, (y * -1) + 0.5, scale, "wallRight.png", true);
-					SpawnManager::getInstance ()->generateMiscSquare (column - floorOffset, row + floorOffset, (y * -1) + 0.5, scale, "barrels.png", true);
-				}
-				else
-				{
-					SpawnManager::getInstance ()->generateMiscSquare (column, row + scale, y * -1, scale, "wallTop.png", true);
-=======
-					SpawnManager::getInstance()->generateMiscSquare(column, row, y * -1, scale, "wallTopRight_Edge.png", true);
-				}
-				else
-				{
-					SpawnManager::getInstance()->generateMiscSquare(column, row, y * -1, scale, "wallHorizontal.png", true);
->>>>>>> 5c2ae033902e886b31a700b29d0dcc98fa50e94e
-				}
-			}
-			// Create bottom walls.
-			else if (y == -size)
-			{
-				if (x == 0)
-				{
-					SpawnManager::getInstance()->generateMiscSquare(column, row, y * -1, scale, "wallBottomLeft.png", true);
-				}
-				else if (x == size)
-				{
-					SpawnManager::getInstance()->generateMiscSquare(column, row, y * -1, scale, "wallBottomRight.png", true);
-				}
-				else
-				{
-<<<<<<< HEAD
-					SpawnManager::getInstance ()->generateMiscSquare (column - 3, row + scale * 2, y * -1, scale, "woodPile.png", true);
-				}
-				SpawnManager::getInstance ()->generateMiscSquare (column, row + (scale / 2), y * -1, scale, "wallBottom.png", true);
-=======
-					SpawnManager::getInstance()->generateMiscSquare(column, row, y * -1, scale, "wallHorizontal.png", true);
-				}
->>>>>>> 5c2ae033902e886b31a700b29d0dcc98fa50e94e
-			}
-			// Create a left wall
-			else if (x == 0)
-			{
-<<<<<<< HEAD
-				SpawnManager::getInstance ()->generateMiscSquare (column - scale + wallOffset, row + floorOffset + scale / 2, y * -1, scale, "wallLeft.png", true);
-				SpawnManager::getInstance ()->generateMiscSquare (column - scale + wallOffset, row + floorOffset, (y * -1) + 0.5, scale, "wallLeft.png", true);
-=======
-				SpawnManager::getInstance()->generateMiscSquare(column, row, y * -1, scale, "wallVerticalBothEdge.png", true);
->>>>>>> 5c2ae033902e886b31a700b29d0dcc98fa50e94e
-			}
-			// Create a right wall.
-			else if (x == size)
-			{
-				float wallHorizontalOffset = 0.0f;
-
-<<<<<<< HEAD
-				SpawnManager::getInstance ()->generateMiscSquare (column + scale - wallOffset, row + floorOffset + scale / 2, y * -1, scale, "wallRight.png", true);
-				SpawnManager::getInstance ()->generateMiscSquare (column + scale - wallOffset, row + floorOffset, (y * -1) + 0.5, scale, "wallRight.png", true);
-			}
-		}
-	}
-	SpawnManager::getInstance ()->generateWorldItem (5, -5, std::make_shared<WoodenShortsword> ());
-=======
-				SpawnManager::getInstance()->generateMiscSquare(column, row, y * -1, scale, "wallVerticalBothEdge.png", true);
-			}
-		}
-	}
-	*/
 	SpawnManager::getInstance()->generateWorldItem(5, -5, std::make_shared<WoodenShortsword>());
-
-	//Ai = SpawnManager::getInstance()->generateAiCharacter(14, -18);
-	//Ai2 = SpawnManager::getInstance()->generateAiCharacter(13, -40);
-	//Ai3 = SpawnManager::getInstance()->generateAiCharacter(50, -50);
-	//Ai4 = SpawnManager::getInstance()->generateAiCharacter(50, -20);
-
-
-
 	Camera::getActiveCamera ()->addComponent<CameraFollow> (Camera::getActiveCamera ().get ());
 	AudioManager::getInstance ()->playMusic (MUSIC_LEVEL_1);
 }
@@ -252,4 +48,34 @@ void NetworkedGameScene::onEnd ()
 
 void NetworkedGameScene::onUpdate (int ticks)
 {
+	if (!menuVisible)
+	{
+		if (InputManager::getInstance()->getKeyState(SDLK_ESCAPE) == InputManager::KeyAction::PRESSED)
+		{
+			menuVisible = true;
+			auto cameraTransform = Camera::getActiveCamera()->getTransform();
+			escapeMenu = GameManager::getInstance()->createGameObject<SimpleSprite>(false, "exitNow.png", cameraTransform->getX(), cameraTransform->getY(), 5000000, 20);
+			//Create menu visibility object and store it
+		}
+	}
+	else {
+		if (InputManager::getInstance()->getKeyState(SDLK_ESCAPE) == InputManager::KeyAction::PRESSED) 
+		{
+			menuVisible = false;
+			escapeMenu->destroy(escapeMenu);
+			escapeMenu = nullptr;
+			//Close menu
+		}
+		else if (InputManager::getInstance()->getKeyState(SDLK_RETURN) == InputManager::KeyAction::PRESSED)
+		{
+			SceneManager::getInstance()->pushScene(new MainMenuScene());
+			//Return to menu
+		}
+		else {
+			auto cameraTransform = Camera::getActiveCamera()->getTransform();
+			escapeMenu->getTransform()->setPosition(cameraTransform->getX(), cameraTransform->getY());
+			//Move menu to camera/center of screen
+		}
+	}
+	
 }
